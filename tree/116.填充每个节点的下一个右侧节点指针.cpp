9f159de27ad73cf1b50b5df8f449059c00f09fd4
hahlw <1031087290@qqcom>
2019-06-30 13:55:30 +0800

    暑假第三天

--- tree/116.填充每个节点的下一个右侧节点指针.cpp
+++ tree/116.填充每个节点的下一个右侧节点指针.cpp
@@ -25,7 +25,33 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        
+        queue<Node*>q;
+        if(!root)return root;
+        Node*cur=root;
+        q.push(root);
+        while(!q.empty()){
+            Node*pre=nullptr;
+            Node*second=nullptr;
+            int qs=q.size();
+            for(int i=0;i<qs;i++){
+                if(i==0){
+                    pre=q.front();
+                }
+                else{
+                    pre->next=q.front();
+                    pre=pre->next;
+                }
+                Node*node=q.front();
+                //cout<<node->val<<endl;
+                if(node->left){
+                    q.push(node->left);
+                    q.push(node->right);
+                }
+                q.pop();
+
+            }
+        }
+        return root;
     }
 };
 
