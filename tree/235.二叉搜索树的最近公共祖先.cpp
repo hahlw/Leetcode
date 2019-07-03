62908da7e0407c257c379f9998c3639db778eaa9
hahlw <1031087290@qqcom>
2019-07-03 21:21:03 +0800

    暑假第六天

--- tree/235.二叉搜索树的最近公共祖先.cpp
+++ tree/235.二叉搜索树的最近公共祖先.cpp
@@ -14,8 +14,14 @@
  */
 class Solution {
 public:
+    /*关键是搜素二叉树，公共祖先一定在两个节点之间。 */
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
+        if (!root) return NULL;
+        if (root->val > max(p->val, q->val)) 
+            return lowestCommonAncestor(root->left, p, q);
+        else if (root->val < min(p->val, q->val)) 
+            return lowestCommonAncestor(root->right, p, q);
+        else return root;
     }
 };
 
