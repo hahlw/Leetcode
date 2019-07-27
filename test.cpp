33d3bf82270d915a3e50cc94545c26b7e2c73672
hahlw <1031087290@qqcom>
2019-07-27 19:07:24 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include <string>
+#include <vector>
+
+int main()
+{
+    string str = "0123";
+    int a = stoi(str);
+    cout << a << endl;
+
+    string b = to_string(a);
+    cout << b;
+    return 0;
+}
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int b = 1;
+    int v = 2;
+    int a = 0;
+    int a = 0;
+}
