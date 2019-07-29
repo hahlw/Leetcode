f08a8663d69e54f3f4916848eafd3f6058b4e110
hahlw <1031087290@qqcom>
2019-07-29 22:12:37 +0800

    Update 90.子集-ii.cpp

--- array/90.子集-ii.cpp
+++ array/90.子集-ii.cpp
@@ -5,35 +5,29 @@
  */
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]=1;
-        }    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums)
+    {
         vector<vector<int>> res;
-        vector<int>cur;
-        sort(nums.begin(),nums.end());
-        helper(nums,res,cur,0,mp);
+        vector<int> cur;
+        sort(nums.begin(), nums.end());
+        helper(nums, res, cur, 0);
         return res;
     }
 
-    void helper(vector<int>&nums,vector<vector<int>>&res,vector<int>&cur,
-                int start,unordered_map<int,int>&mp){
-        if(start>=nums.size()){
+    void helper(vector<int>& nums, vector<vector<int>>& res, vector<int>& cur,
+        int start)
+    {
+        if (start >= nums.size()) {
             res.push_back(cur);
-            return ;
+            return;
         }
         res.push_back(cur);
-        for(int i=start;i<nums.size();i++){
-            if(mp[nums[i]]==1&&i>0&&nums[i]==nums[i-1])
+        for (int i = start; i < nums.size(); i++) {
+            if (i > start && nums[i] == nums[i - 1])
                 continue;
             cur.push_back(nums[i]);
-            mp[nums[i]]=0;
-            helper(nums,res,cur,i+1,mp);
+            helper(nums, res, cur, i + 1);
             cur.pop_back();
-            mp[nums[i]]=1;
         }
     }
 };
-
-
